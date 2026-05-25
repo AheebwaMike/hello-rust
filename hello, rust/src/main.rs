d06f59cfa84c145bb7afdf2b7f685f3c8aab7d7a
Michael Aheebwa <michaelheebwa357@gmail.com>
2026-05-25 14:48:03 +0300

    new: my first rust program

---
+++ hello, rust/src/main.rs
@@ -0,0 +1,9 @@
+// This is my first rust program
+
+fn main() {
+    println!("\n-----------------------");
+    println!("\nHello, rust\n");
+    println!("I'm coming for you...\n");
+    println!("\t - Aheebwa");
+    println!("-----------------------\n");
+}
