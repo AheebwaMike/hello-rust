27b29e5428ad6a44271149292d5ccc4d409fa1c5
Michael Aheebwa <michaelheebwa357@gmail.com>
2026-08-09 00:39:27 +0300

    foundations: implemented a rust function with parameters

--- hello, rust/src/main.rs
+++ hello, rust/src/main.rs
@@ -1,9 +1,35 @@
-// This is my first rust program
+// // This is my first rust program
+
+const CONCEPTS: (&str, &str) = (
+    "Mutability",
+    "Datatypes"
+);
+
+// Terminal decorator function
+fn decor_up(fn_name: &str, _is_end: bool) {
+    println!("\n============================");
+    println!("Running code on: {fn_name}");
+    println!("============================");
+}
+
+fn decor_down() {
+    println!("----------------------------\n");
+}
+
+// mutability
+fn mutability() {
+    decor_up(CONCEPTS.0, false);
+    let name = "Mike";
+    let mut age = 22;
+    println!();
+    println!("His name is {name} and he's {age} years old");
+
+    age = 36;
+    println!("His new age now {age}\n");
+    decor_down()
+}
 
 fn main() {
-    println!("\n-----------------------");
-    println!("\nHello, rust\n");
-    println!("I'm coming for you...\n");
-    println!("\t - Aheebwa");
-    println!("-----------------------\n");
+    mutability();
+    mutability();
 }
