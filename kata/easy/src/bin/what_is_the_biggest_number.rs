bb8de1c0fd95df8c6ff5027c54e39704601e1c36
Michael Aheebwa <michaelheebwa357@gmail.com>
2026-08-18 00:20:29 +0300

    kata solution: largest number in array of numbers

--- kata/easy/src/bin/what_is_the_biggest_number.rs
+++ kata/easy/src/bin/what_is_the_biggest_number.rs
@@ -1,9 +1,10 @@
 
 fn main() {
     // Enter your list of numbers here
-    let numbers = [2, 4, 5, 6, 8, 125, 43, 123, 7];
+    let numbers: [u32; 4] = [2, 1, 7, 3];
 
     let mut biggest = 0;
+    
     for n in numbers {
         if n > biggest {
             biggest = n;
